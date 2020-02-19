f0973377b8609e1ccd321bcc5457598f2bc7a920
Logan <02logan.pennock@gulllakecs.org>
2020-02-19 09:32:18 -0500

    add corvette and destroyer

--- app/src/main/java/com/example/lesson5/MainActivity.java
+++ app/src/main/java/com/example/lesson5/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.lesson5;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.android.cookies;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
     }
 }
